b7fe070becae79b502cb336d9663da43336bb0d9
uminur28 <32719946+uminur28@users.noreply.github.com>
2017-11-16 11:40:38 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$("#light-slider").lightSlider({
+  item: 6,
+  loop: true,
+  pager: false,
+  auto:true,
+  pauseOnHover: true,
+  responsive: [{
+    breakpoint: 600,
+    settings: {
+      item: 1
+    }
+  }]
+});
